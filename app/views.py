5954bce6c72fa58acecf0b72adb0c8a97cb63aa4
aruntakkar <aruntakkar5@gmail.com>
2016-08-08 23:54:53 +0530

    app is done need to add Search Filters

--- app/views.py
+++ app/views.py
@@ -7,21 +7,15 @@ import json
 
 
 def index(request):
-    json_response = []
+    hospitals_list = []
     for i in range(30):
         response = requests.get(
             'https://data.gov.in/api/datastore/resource.json?resource_id=e16c75b6-7ee6-4ade-8e1f-2cd3043ff4c9&api-key=' + settings.API_KEY + '&offset=' + str(i))
-        json_response.append(response.json()['records'])
+        hospitals_list.append(response.json()['records'])
 
     parseData = []
-    spec_info = {}
-    for each_dict in json_response:
-        for each_item in each_dict:
-            spec_info['District'] = each_item['district']
-            spec_info['Address'] = each_item['address']
-            spec_info['Hospital'] = each_item['h_name']
-            spec_info['State'] = each_item['state']
-            spec_info['Contact'] = each_item['contact']
-            spec_info['id'] = each_item['f_id']
-        parseData.append(spec_info)
-    return render(request, 'app/index.html', {'data': parseData})
+    for list_counter in range(len(hospitals_list)):
+        for each_list in hospitals_list:
+            for each_dict in each_list:
+                parseData.append(each_dict)
+        return render(request, 'app/index.html', {'data': parseData})
